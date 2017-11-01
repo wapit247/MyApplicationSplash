f357d0d18ca974a30fd440f54ee0842b27479cdf
AGreenhalgh <Alex.Greenhalgh@three.co.uk>
2017-11-01 17:10:16 +0000

    Added Drop down list for which Office

--- app/src/main/java/com/wapit/myapplicationsplash/MainActivity.java
+++ app/src/main/java/com/wapit/myapplicationsplash/MainActivity.java
@@ -1,13 +1,42 @@
 package com.wapit.myapplicationsplash;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements OnItemSelectedListener {
+
+    Spinner spinnerDialog;
+    ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        adapter = ArrayAdapter.createFromResource(this,R.array.office_options,android.R.layout.simple_spinner_item);
+
+        spinnerDialog = (Spinner) findViewById(R.id.Office);
+        spinnerDialog.setAdapter(adapter);
+        spinnerDialog.setOnItemSelectedListener(MainActivity.this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        TextView spinnerDialogText = (TextView) view;
+        Toast.makeText(this, "you selected " + spinnerDialogText.getText(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
+
